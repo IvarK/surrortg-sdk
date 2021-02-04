6e9ffc08fc7f6e110d342e6906680574b897228a
juhopaakkanen <juho.paakkanen@gmail.com>
2021-02-04 15:49:00 +0200

    Added files.

---
+++ games/gps_car/test_server.py
@@ -0,0 +1,30 @@
+import asyncio
+import socketio
+from aiohttp import web
+
+#Test server for a gps game
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+areaData = [(0,0),(0, 10), (10,10), (10,0)]
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+    asyncio.run_coroutine_threadsafe(
+        sio.emit('boundary_data', {'data': areaData}),
+        asyncio.get_event_loop(),
+    )
+
+@sio.event
+async def update_location(sid, data):
+    print(f"received data :{data}")
+
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == "__main__":
+    web.run_app(app, port=9090)
