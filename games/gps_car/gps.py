b97c40731804662016ce5202c7ef1cad9f34cc6d
juhopaakkanen <juho.paakkanen@gmail.com>
2021-02-21 19:19:43 +0200

    local_testing

--- games/gps_car/gps.py
+++ games/gps_car/gps.py
@@ -71,11 +71,11 @@ class GPSSocket:
     sio = socketio.AsyncClient()
     secret = "asd"
 
-    def __init__(self, url, robot_id, game_id, gps_area):
+    def __init__(self, url, robot_id, game_id:
         self.url = url
         self.robot_id = robot_id
         self.game_id = game_id
-        self.gps_area = gps_area
+        self.gps_area = GPSArea([(0,0),(0, 6), (6,6), (6,0)])
 
     
     @sio.event(namespace='/robot')
