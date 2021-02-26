31e5fd810757c5dbdabb1e65b974af24573808fa
juhopaakkanen <juho.paakkanen@gmail.com>
2021-02-27 01:36:42 +0200

    testing pre commit hooks

--- games/gps_car/gps_test.py
+++ games/gps_car/gps_test.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-import time
 import serial
+
 ser = serial.Serial(
-        port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
+    port="/dev/ttyS0",  # Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1,
 )
 while 1:
-    x=ser.readline()
+    x = ser.readline()
     print(x)
